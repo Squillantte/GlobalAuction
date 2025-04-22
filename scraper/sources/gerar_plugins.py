360227be913810a241e002cd8178f6af770b6a8a
Squillantte <gabrielsquillantte@gmail.com>
2025-04-22 18:57:02 -0300

    Update and rename python gerar_plugins.py to gerar_plugins.py

--- scraper/sources/gerar_plugins.py
+++ scraper/sources/gerar_plugins.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 estados = [
     "AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT",
-    "PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"
+    "PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","TO"
 ]
 
 def criar(uf: str, modelo: Path):
